9d1ce5269658ffce0c277185137bda34a740b558
Victor <vvfagbolu@gmail.com>
2021-08-09 13:37:47 -0500

    Initial Commit

---
+++ Breaking Bad Fan App/Breaking_Bad_Fan_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Breaking_Bad_Fan_AppApp.swift
+//  Breaking Bad Fan App
+//
+//  Created by COE on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Breaking_Bad_Fan_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Breaking Bad Fan App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Breaking Bad Fan App
+//
+//  Created by COE on 8/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
